5ef3e0ada552bd3bba6310eed7d1a0c28f07a188
Rafiul901 <rafiuldipu2480@gmail.com>
2026-02-10 21:43:08 +0600

    new

--- script.js
+++ script.js
@@ -3,6 +3,40 @@ const loadLessons=()=>{
     .then((res)=>res.json())
     .then((json)=>displayLesson(json.data));
 }
+// button function
+const loadLevelWord=(id)=>{
+const url =`https://openapi.programming-hero.com/api/level/${id}`;
+fetch (url)
+.then((res)=>res.json())
+.then((data)=>displayLevelWord(data.data))
+}
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = '';
+
+  words.forEach(word => {
+    const card = document.createElement('div');
+
+    card.innerHTML = `
+      <div class="max-w-xs mx-auto bg-white border border-gray-300 rounded-lg overflow-hidden">
+
+  <div class="p-6 text-center border-b border-gray-200">
+    <h2 class="text-3xl font-bold text-gray-800">${word.word}</h2>
+   
+  </div>
+
+  <div class="p-6 text-center">
+    <p class="text-2xl font-semibold text-gray-900">${word.meaning}</p>
+   
+  </div>
+
+</div>
+    `;
+
+    wordContainer.append(card);
+  });
+};
 
 
 const displayLesson =(lessons) =>{
@@ -12,9 +46,8 @@ levelContainer.innerHTML='';
 for(let lesson of lessons){
     const btnDiv= document.createElement('div');
     btnDiv.innerHTML=`
-      <button
-                        class="btn bg-white hover:bg-blue-600 text-black hover:text-white border-blue-800">${lesson.lessonName}</button>`
-                        levelContainer.append(btnDiv)
+      <button onclick="loadLevelWord(${lesson.level_no})" class="btn bg-white hover:bg-blue-600 text-black hover:text-white border-blue-800">${lesson.lessonName}</button>`
+     levelContainer.append(btnDiv)
 
 }
 }
